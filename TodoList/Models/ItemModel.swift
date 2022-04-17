6e95ee89bc465b442584c7c33075052ac67bcf0d
mukhammad <muhammadmnnm1@gmail.com>
2022-04-17 23:05:22 +0500

    Create a custom data model for Todo items

--- TodoList/Models/ItemModel.swift
+++ TodoList/Models/ItemModel.swift
@@ -6,9 +6,22 @@
 //
 
 import Foundation
+import SwiftUI
+
+//Immutable struct
 
 struct ItemModel: Identifiable {
-  let id: String = UUID().uuidString
+  let id: String
   let title: String
   let isCompleted: Bool
+
+  init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+    self.id = id
+    self.title = title
+    self.isCompleted = isCompleted
+  }
+
+  func updateCompletion() -> ItemModel {
+    return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+  }
 }
